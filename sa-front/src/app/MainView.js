75ee233fe098480c572fe63451e8a5a40dd368d7
nahuel <nahuelblangetti@gmail.com>
2022-10-15 12:00:35 -0300

    fix views

--- sa-front/src/app/MainView.js
+++ sa-front/src/app/MainView.js
@@ -1,6 +1,5 @@
 import Turnos from './pages/Turnos';
 import Presentation from './pages/Presentation';
-import Header from '../app/Header';
 import SidebarLeft from '../app/SidebarLeft';
 import tailwindcss from "../index.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
@@ -9,13 +8,7 @@ import { Footer, Progress, Sidebar } from 'flowbite-react';
 export default function MainView() {
   return (
     <div>
-        <SidebarLeft/>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/presentacion' element={<Presentation/>} exact></Route>
-                <Route path='/turnos' element={<Turnos/>} exact></Route>
-            </Routes>
-        </BrowserRouter>
+      <SidebarLeft/>
     </div>
   )
 }
